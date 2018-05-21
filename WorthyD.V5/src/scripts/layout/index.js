7188131c6e16301344b46d6c88ccbfea9f58b905
Daniel Worthy <daniel.worthy@vml.com>
2018-05-21 13:52:04 -0500

    random updates

--- WorthyD.V5/src/scripts/layout/index.js
+++ WorthyD.V5/src/scripts/layout/index.js
@@ -1,6 +1,9 @@
-var RandomBG = (function () {
+var HomeScreen = (function () {
     var _Init = function (elmId) {
-
+        var infoBlock = document.querySelectorAll(elmId);
+        if (infoBlock.length > 0) {
+            infoBlock[0].classList.add('bg-' + (Math.floor(Math.random() * 11) + 1));
+        }
     }
 
     return {
