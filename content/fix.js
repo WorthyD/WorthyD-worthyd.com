05657f583479fc9c40077b54161174cf9bfd3516
WorthyD <danieljworthy@gmail.com>
2021-12-08 17:09:41 -0600

    STuff

--- content/fix.js
+++ content/fix.js
@@ -3,60 +3,20 @@
 const replace = require('replace-in-file');
 
 const options = {
-    files: 'blog/201*/**/*.md',
-    from: /date: (.+?)* /g,
+    files: 'blog/**/*.md',
+     from: /date: (.+?)* /g,
+    // from: /---\n(.+?\n)*---\n/gm,
+    //from: /---\n([\s\S]*?)--\n/,
     to: (match) => {
-        const x =  match.split(': ')
-        const newThing = `${match}\ndatePublished: ${x[1]} \ndateModified: ${x[1]}`
-        return newThing;
+      console.log('hello');
+      // console.log(match);
+        return match + 'd';
     },
+//    dry:true,
+    countMatches: true,
   };
-  console.log('stuff');
-
-//   const results = replace.sync(options);
-  
-//   console.log(results);
 
   replace(options)
-  .then(results => {
-    console.log('Replacement results:', results);
-  })
-  .catch(error => {
-    console.error('Error occurred:', error);
-  });
-const options2 = {
-    files: 'blog/201*/**/*.md',
-    from: /npublishDate: (.+?)* /g,
-    to: (match) => {
-        return '';
-    },
-  };
-  console.log('stuff');
-
-//   const results = replace.sync(options);
-  
-//   console.log(results);
-
-  replace(options2)
-  .then(results => {
-    console.log('Replacement results:', results);
-  })
-  .catch(error => {
-    console.error('Error occurred:', error);
-  });
-  const options3 = {
-    files: 'blog/201*/**/*.md',
-    from: /nmodifiedDate: (.+?)* /g,
-    to: (match) => {
-        return '';
-    },
-  };
-
-//   const results = replace.sync(options);
-  
-//   console.log(results);
-
-  replace(options3)
   .then(results => {
     console.log('Replacement results:', results);
   })
